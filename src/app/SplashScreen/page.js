b35667c22d29126f5f36cfbc15c07bd2f18436d2
Lani Do <lani.do@sjsu.edu>
2025-05-14 17:11:05 -0700

    capitalize file names

--- src/app/SplashScreen/page.js
+++
@@ -1,9 +0,0 @@
-export default function page() {
-  return (
-    <div>
-      <h1 className="text-4xl font-bold text-center mt-10">Splash Screen</h1>
-      {/* Logo */}
-      {/* Fade Animation into homepage */}
-    </div>
-  )
-}
